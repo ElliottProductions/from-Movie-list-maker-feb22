620651f55800a8def25a56141aaf7e88ddb4282f
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-05-06 15:05:41 -0800

    deleted console statement

--- src/MovieList.js
+++ src/MovieList.js
@@ -4,7 +4,6 @@ import Movie from './Movie';
 export default function MovieList({ allMovies, handleDeleteMovie }) {
   return (
     <div className="right">
-      {console.log(allMovies)}
       {allMovies.map((movie) => <Movie key={movie.movieTitle + Math.floor(Math.random() * 1000)} movie={movie} handleDeleteMovie={handleDeleteMovie}
       />)}
       
